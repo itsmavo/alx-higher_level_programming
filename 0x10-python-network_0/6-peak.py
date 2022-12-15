90000863de66715483428a5f52f6a735d6270923
itsmavo <mansolo.osolo51@gmail.com>
2022-12-15 12:03:18 -0800

    Curling Iron

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# finds peaks
+
+def search(lo, h, ints):
+    mid = (lo + h) // 2
+    if lo == h:
+        return ints[h]
+    if ints[mid] < ints[mid + 1]:
+        return(search(mid + 1, h, ints))
+    return(search(lo, mid, ints))
+
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return
+    return(search(0, len(list_of_integers) - 1, list_of_integers))
