e203c099833cbc28d29d10eced6db6d752c64231
Marvin Osolo <51908179+itsmavo@users.noreply.github.com>
2023-05-23 16:48:49 +0300

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@ let request = require('request');
 let adress = process.argv[2];
 request(adress, function (error, response, body) {
     if(error){
-        console.log(error)
+        console.log(error);
     } else {
         console.log('code:', response['statusCode']);
     }
