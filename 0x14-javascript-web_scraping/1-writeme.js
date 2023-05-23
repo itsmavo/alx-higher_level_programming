091aeb09b2df34a190a4c510dcd09c8a8485c203
Marvin Osolo <51908179+itsmavo@users.noreply.github.com>
2023-05-23 16:53:19 +0300

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-
 let file = process.argv[2];
 let string = process.argv[3];
 let fs = require('fs');
+
 fs.writeFile(file, string, function (err) {
     if(err) {
         console.log(err);
